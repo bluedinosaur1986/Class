c1d33b58cb1a320e1c3168cadc6da02a472bd51f
bluedinosaur1986 <156623454+bluedinosaur1986@users.noreply.github.com>
2024-04-17 18:34:46 -0400

    Add files via upload

---
+++ db.py
@@ -0,0 +1,32 @@
+def Menu():
+    print("Main Menu:")
+    print("1. Insert a record")
+    print("2. Read all records")
+    selection=input("Input your choice:")
+    if selection=="1":
+        insertStuff
+    elif selection=="2":
+        readStuff
+    
+
+def insertStuff():
+    myDate=input("Input date:")
+    myTX=input("Transaction type:")
+    mySymbol=input("Input ticker symbol:")
+    myQty=float(input("Enter the quantity:"))
+    myPrice=float(input("Enter price:"))
+    data.execute("INSERT INTO stocks VALUES (myDate, myTX, mySymbol, myQty, myPrice)")
+    conn.commit()
+
+def readStuff():
+    data.execute('SELECT * FROM stocks')
+    for row in data.fetchall():
+        print(row)
+
+import sqlite3
+conn=sqlite3.connect('example.db')
+data=conn.cursor()
+data.execute('''create table if not exists stocks (date text, trans text, symbol text, qty real, price real)''')
+conn.commit()
+Menu
+conn.close()
